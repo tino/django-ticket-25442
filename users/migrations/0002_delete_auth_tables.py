b231c1a86a84e729a2814deb662451bcbf9f060a
Tino de Bruijn <tinodb@gmail.com>
2015-09-22 21:10:18 +0200

    Formatting

--- users/migrations/0002_delete_auth_tables.py
+++ users/migrations/0002_delete_auth_tables.py
@@ -12,11 +12,11 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
-                          "DROP TABLE auth_user_user_permissions;"
-                          "DROP TABLE auth_group_permissions;"
-                          "DROP TABLE auth_user_groups;"
-                          "DROP TABLE auth_group;"
-                          "DROP TABLE auth_permission;"
-                          "DROP TABLE auth_user;"
-                          )
+            "DROP TABLE auth_user_user_permissions;"
+            "DROP TABLE auth_group_permissions;"
+            "DROP TABLE auth_user_groups;"
+            "DROP TABLE auth_group;"
+            "DROP TABLE auth_permission;"
+            "DROP TABLE auth_user;"
+        )
     ]
